b2e07e4ae3117054fcb968f4259862f208cc12b4
oriss-dev <orissolorzano@gmail.com>
2025-12-05 00:43:38 -0300

    docker integrado

---
+++ micro-usuario-App-franco-integrar/usuario/src/main/java/com/micro/usuario/Config/SwaggerConfig.java
@@ -0,0 +1,36 @@
+package com.micro.usuario.Config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.models.OpenAPI;
+
+@Configuration
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Usuario API",
+                version = "1.0",
+                description = "API REST para gestión de usuarios con autenticación JWT"
+        ),
+        security = {
+            @SecurityRequirement(name = "bearerAuth")}
+)
+@SecurityScheme(
+        name = "bearerAuth",
+        type = SecuritySchemeType.HTTP,
+        scheme = "bearer",
+        bearerFormat = "JWT"
+)
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI();
+    }
+
+}
